94f55000bd89b287ded5d7dd6ee51f201f5b0546
Angad Kambli <angadkambli@gmail.com>
2024-07-21 04:01:28 +0530

    make your enemies burn

--- enemies/defaultEnemy.gd
+++ enemies/defaultEnemy.gd
@@ -4,8 +4,10 @@ extends CharacterBody3D
 @export_enum("default", "crazy") var enemy_type = "default"
 @export_node_path("CharacterBody3D") var targetCharacter
 var target:CharacterBody3D
-var isHit = true
+var isHit = false
 var initial_velocity
+var time_until_death = 5
+var total_time_until_death = time_until_death
 
 func _ready():
 	if(enemy_type=="default"):
@@ -14,10 +16,10 @@ func _ready():
 		velocity = ($moveTarget.position - position).normalized() * speed
 		initial_velocity = velocity
 	pass
-
-func hit():
-	isHit = true
-	pass
+	
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		burn()
 
 func _physics_process(delta):
 	#if(enemy_type == "crazy"):
@@ -39,6 +41,18 @@ func _process(delta):
 		if(enemy_type == "crazy"):
 			var x = randf_range(0, 1000)
 			velocity = initial_velocity.rotated(Vector3(0, 1, 0), x).normalized() * speed
+	
+	if(isHit):
+		time_until_death -= delta
+		$MeshInstance3D.get_surface_override_material(0).albedo_color.a = time_until_death/total_time_until_death
+		if(time_until_death < 1):
+			$Fire/GPUParticles3D.emitting = false
+		#$MeshInstance3D.get_surface_override_material(0).albedo_color.a = 0
+		#$MeshInstance3D.surface_material_override.albedo_color.a = lerp($MeshInstance3D.surface_material_override.albedo_color.a, 0, 0.05)
+		if(time_until_death <= 0):
+			queue_free()
+		
 
-func rekt():
-	print("Enemy is kil")
+func burn():
+	$Fire/GPUParticles3D.emitting = true
+	isHit = true
