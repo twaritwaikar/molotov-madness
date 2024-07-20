1930fade2ab0ae1d6ef5ff1bb062c7baed031dde
Angad Kambli <angadkambli@gmail.com>
2024-07-20 20:27:39 +0530

    lights

---
+++ lights/rotating_lights.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@export var speed = 10
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate_y(10*delta)
+	pass
