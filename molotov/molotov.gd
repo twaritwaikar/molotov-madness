b863b5519d95cfc9dc0d4ed011eca95dd3a4f5cf
Twarit Waikar <wtwarit@gmail.com>
2024-07-21 04:07:02 +0530

    Add sfx + molo cleanup

--- molotov/molotov.gd
+++ molotov/molotov.gd
@@ -1,19 +1,34 @@
 extends RigidBody3D
 
 var base_angular_velocity = 50
+var has_hit_the_floor = false
 
 func _ready():
 	angular_velocity = Vector3(randf_range(0.5, 1.0), randf_range(0.5, 1.0), randf_range(0.5, 1.0)).normalized()
 	angular_velocity *= base_angular_velocity
+	$ThrowAudioStream.play()
 
 func _physics_process(delta):
 	pass
 
-func _on_body_entered(body):
-	if body.has_method("start_burning"):
-		body.start_burning()
+func _process(delta):
+	print($ExplodeAudioStream.playing)
 
-	# Get enemies in radius
-	for overlapping_body in $Area3D.get_overlapping_bodies():
-		if overlapping_body.get_meta("is_enemy", false) == true:
-			overlapping_body.rekt()
+func _on_body_entered(body):
+	if has_hit_the_floor:
+		return
+		
+	if body.get_meta("is_floor", false) == true:
+		print("Floor hit!")
+		# Start burning effects
+		# Get enemies in radius
+		for overlapping_body in $Area3D.get_overlapping_bodies():
+			if overlapping_body.get_meta("is_enemy", false) == true:
+				overlapping_body.rekt()
+
+		if $ThrowAudioStream.playing:
+			$ThrowAudioStream.stop()
+		if !$ExplodeAudioStream.playing:
+			$ExplodeAudioStream.play()
+	else:
+		$BounceAudioStream.play()
